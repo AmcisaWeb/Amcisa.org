9d3543582e1e72615a16219d9bec3e91650313ca
sgclreadingcompetition <sgclreadingcompetition@gmail.com>
2018-03-06 03:38:44 +0800

    Test

--- Deploy.bat
+++ Deploy.bat
@@ -3,3 +3,7 @@ COPY Amcisa-Vue\dist\index.html Amcisa-Laravel\resources\views\welcome.blade.php
 del Amcisa-Laravel\public_html\static\* /S /Q
 
 XCOPY Amcisa-Vue\dist\static\* Amcisa-Laravel\public_html\static /s /i
+
+del Amcisa-Laravel\bootstrap\cache\* /S /Q
+
+copy NUL Amcisa-Laravel\bootstrap\cache\.gitkeep
