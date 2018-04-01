c27c19ad87477f3e4d6b4da9969c4e8bcb0eea07
kimyong95 <kimyong95@gmail.com>
2018-04-01 16:17:30 +0800

    Fix Bugs for Video Field

--- Amcisa-Vue/src/components/pages/fields/Video.vue
+++ Amcisa-Vue/src/components/pages/fields/Video.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <video width="400" controls>
-      <source src="http://amcisa.org/api/download/32_Am斯卡提名/15220610438331.MOV">
+      <source :src="$store.state.baseUrl + '/api/download' + field.filename">
       Your browser does not support HTML5 video.
     </video>
   </div>
