ed95c98ebb49cc248d95660e720ba214ca38f57c
kimyong95 <kimyong95@gmail.com>
2018-04-02 04:40:55 +0800

    Fix DropDown Bugs

--- Amcisa-Laravel/resources/views/welcome.blade.php
+++ Amcisa-Laravel/resources/views/welcome.blade.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>amcisa-vue</title><link href=/static/css/app.25352c2d355d588c5cd93d4f4cc4e37d.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.3d2e4c6d062ad50ed719.js></script><script type=text/javascript src=/static/js/vendor.adbea74b8741df857db9.js></script><script type=text/javascript src=/static/js/app.5be2c0df8b2ef2531a6d.js></script></body><style>body{
-=======
-<!DOCTYPE html><html><head><meta charset=utf-8><meta name=viewport content="width=device-width,initial-scale=1"><title>amcisa-vue</title><link href=/static/css/app.25352c2d355d588c5cd93d4f4cc4e37d.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.8f2ac22993c5835b37e4.js></script><script type=text/javascript src=/static/js/vendor.adbea74b8741df857db9.js></script><script type=text/javascript src=/static/js/app.deb1117770884f358e59.js></script></body><style>body{
->>>>>>> parent of c27c19a... Fix Bugs for Video Field
       background-color: lightgrey;
     }</style></html>
