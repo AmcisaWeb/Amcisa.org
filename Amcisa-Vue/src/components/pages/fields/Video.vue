d489322f2779d7845adf24d5925d713bc45c29cd
kimyong95 <kimyong95@gmail.com>
2018-04-02 04:38:22 +0800

    Revert "Fix Bugs for Video Field"
    This reverts commit c27c19ad87477f3e4d6b4da9969c4e8bcb0eea07.

--- Amcisa-Vue/src/components/pages/fields/Video.vue
+++ Amcisa-Vue/src/components/pages/fields/Video.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <video width="400" controls>
-      <source :src="$store.state.baseUrl + '/api/download' + field.filename">
+      <source src="http://amcisa.org/api/download/32_Am斯卡提名/15220610438331.MOV">
       Your browser does not support HTML5 video.
     </video>
   </div>
